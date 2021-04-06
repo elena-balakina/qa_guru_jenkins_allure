e622a8d690367f90ec0e0ca23808c8d5b93c486a
vera-ПК\vera <e.p.balakina@mail.ru>
2021-04-06 22:42:54 +0700

    Implement tests for Student registration form

---
+++ src/test/java/tests/StudentRegistrationFormTests.java
@@ -0,0 +1,28 @@
+package tests;
+
+import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class StudentRegistrationFormTests extends TestBase {
+    private WebSteps step = new WebSteps();
+
+    @Test
+    @Owner("ebalakina")
+    @Feature("Practice form")
+    @Story("Student registration")
+    @DisplayName("Fill the form and check Results table")
+    @Severity(SeverityLevel.BLOCKER)
+    @Link(name = "URL", url = "https://demoqa.com/automation-practice-form")
+    public void practiceFormTest() {
+        step.openPracticeFromPage();
+        step.fillCommonInformation();
+        step.setBirthDate();
+        step.addSubjects();
+        step.addHobbies();
+        step.uploadPicture();
+        step.fillAddress();
+        step.submitForm();
+        step.checkResultsTable();
+    }
+}
